9f9d5357030b91f337be8d8104d27d5a2ae05b20
Jens Wille <jens.wille@uni-koeln.de>
2010-07-01 12:13:32 +0200

    Refactoring and documentation.
    * Introduced Jekyll::Engine::Erb::Helpers module
    * Provide aliases to original methods

--- lib/jekyll/rendering.rb
+++ lib/jekyll/rendering.rb
@@ -36,6 +36,9 @@ module Jekyll
 
   module Convertible
 
+    alias_method :_rendering_original_do_layout, :do_layout
+
+    # Overwrites the original method to use the configured rendering engine.
     def do_layout(payload, layouts)
       info = { :filters => [Jekyll::Filters], :registers => { :site => site } }
 
@@ -58,6 +61,11 @@ module Jekyll
       end
     end
 
+    # call-seq:
+    #   engine => aClass
+    #
+    # Returns the Engine class according to the configuration setting for
+    # +engine+ (see subclasses of Engine::Base). Defaults to Engine::Liquid.
     def engine
       @engine ||= Engine[site.config['engine'] ||= 'liquid']
     end
@@ -66,12 +74,20 @@ module Jekyll
 
   module Engine
 
+    # call-seq:
+    #   Engine[engine] => aClass
+    #
+    # Returns the subclass whose name corresponds to +engine+.
     def self.[](engine)
       const_get(engine.capitalize)
     end
 
     class Base
 
+      # call-seq:
+      #   Engine::Base.render(*args)
+      #
+      # Renders the output. Defers to engine's render method.
       def self.render(payload, content, info, layout = nil)
         new(payload, content, info).render(layout || content)
       end
@@ -83,6 +99,10 @@ module Jekyll
         @payload, @content, @info = payload, content, info
       end
 
+      # call-seq:
+      #   render
+      #
+      # Renders the output. Must be implemented by subclass.
       def render
         raise NotImplementedError
       end
@@ -91,6 +111,12 @@ module Jekyll
 
     class Liquid < Base
 
+      # call-seq:
+      #   engine.render
+      #   engine.render(content)
+      #
+      # Renders the +content+ using ::Liquid::Template::parse and then
+      # calling ::Liquid::Template#render with +payload+ and +info+.
       def render(content = content)
         ::Liquid::Template.parse(content).render(payload, info)
       end
@@ -104,7 +130,7 @@ module Jekyll
       def initialize(*args)
         super
 
-        info[:filters].each { |filter| extend filter }
+        [Helpers, *info[:filters]].each { |mod| extend mod }
 
         %w[site page paginator].each { |key|
           value = payload[key] or next
@@ -112,58 +138,81 @@ module Jekyll
         }
       end
 
+      # call-seq:
+      #   engine.render => aString
+      #   engine.render(content) => aString
+      #   engine.render(content, binding) => aString
+      #
+      # Renders the +content+ as ERB template. Uses optional +binding+
+      # if provided.
       def render(content = content, binding = binding)
         ::ERB.new(content).result(binding)
       end
 
-      def include_file(file, binding = binding)
-        Dir.chdir(File.join(site.source, '_includes')) {
-          @choices ||= Dir['**/*'].reject { |x| File.symlink?(x) }
+      module Helpers
 
-          if @choices.include?(file = file.strip)
-            render(File.read(file), binding)
-          else
-            "Included file '#{file}' not found in _includes directory"
-          end
-        }
-      end
+        # call-seq:
+        #   include_file file => aString
+        #   include_file file, binding => aString
+        #
+        # Includes file +file+ from <tt>_includes</tt> directory rendered
+        # as ERB template. Uses optional +binding+ if provided.
+        def include_file(file, binding = binding)
+          Dir.chdir(File.join(site.source, '_includes')) {
+            @choices ||= Dir['**/*'].reject { |x| File.symlink?(x) }
 
-      def highlight(text, lang = :ruby)
-        if site.pygments
-          render_pygments(text, lang)
-        else
-          render_codehighlighter(text, lang)
+            if @choices.include?(file = file.strip)
+              render(File.read(file), binding)
+            else
+              "Included file '#{file}' not found in _includes directory"
+            end
+          }
         end
-      end
 
-      protected
+        # call-seq:
+        #   highlight text => aString
+        #   highlight text, lang => aString
+        #
+        # Highlights +text+ according to +lang+ (defaults to Ruby).
+        def highlight(text, lang = :ruby)
+          if site.pygments
+            render_pygments(text, lang)
+          else
+            render_codehighlighter(text, lang)
+          end
+        end
 
-      def render_pygments(text, lang)
-        output = add_code_tags(Albino.new(text, lang).to_s, lang)
-        "#{payload['pygments_prefix']}#{output}#{payload['pygments_suffix']}"
-      end
+        protected
 
-      def render_codehighlighter(text, lang)
-        #The div is required because RDiscount blows ass
-        <<-HTML
-<div>
-  <pre>
-    <code class="#{lang}">#{h(text).strip}</code>
-  </pre>
-</div>
-        HTML
-      end
+        def render_pygments(text, lang)
+          output = add_code_tags(Albino.new(text, lang).to_s, lang)
+          "#{payload['pygments_prefix']}#{output}#{payload['pygments_suffix']}"
+        end
 
-      def add_code_tags(code, lang)
-        # Add nested <code> tags to code blocks
-        code.sub(%r{<pre>},  %Q{<pre><code class="#{lang}">}).
-             sub(%r{</pre>}, %q{</code></pre>})
+        def render_codehighlighter(text, lang)
+          #The div is required because RDiscount blows ass
+          <<-HTML
+  <div>
+    <pre>
+      <code class="#{lang}">#{h(text).strip}</code>
+    </pre>
+  </div>
+          HTML
+        end
+
+        def add_code_tags(code, lang)
+          # Add nested <code> tags to code blocks
+          code.sub(%r{<pre>},  %Q{<pre><code class="#{lang}">}).
+               sub(%r{</pre>}, %q{</code></pre>})
+        end
+
       end
 
     end
 
   end
 
+  # Provide an engine-agnostic name for the hash representation.
   [Post, Page, Pager].each { |klass|
     klass.send(:alias_method, :to_hash, :to_liquid)
   }
