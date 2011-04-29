7ca26ee3f6460993ea3c24b3f17d2112cfa0518b
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 17:24:52 +0200

    Switched license to Affero GPL.

--- lib/jekyll/rendering.rb
+++ lib/jekyll/rendering.rb
@@ -11,17 +11,17 @@
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # jekyll-rendering is free software; you can redistribute it and/or modify it #
-# under the terms of the GNU General Public License as published by the Free  #
-# Software Foundation; either version 3 of the License, or (at your option)   #
-# any later version.                                                          #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
 #                                                                             #
 # jekyll-rendering is distributed in the hope that it will be useful, but     #
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for #
-# more details.                                                               #
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public      #
+# License for more details.                                                   #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with jekyll-rendering. If not, see <http://www.gnu.org/licenses/>.          #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with jekyll-rendering. If not, see <http://www.gnu.org/licenses/>.    #
 #                                                                             #
 ###############################################################################
 #++
